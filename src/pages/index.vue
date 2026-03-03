14166efcfd3dbd97944cc7f6e7fc8e07012cc995
nailao <mr.wutangnailao@gmail.com>
2026-03-03 16:01:02 +0800

    fix: route logged-in homepage to article editor

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,11 @@
 <template>
-  <component :is="FriendsIndex"/>
+  <component :is="currentPage" />
 </template>
 
 <script setup lang="ts">
-import DefaultIndex from "./defaultIndex.vue"
-import FriendsIndex from "./friendsIndex.vue"
+import DefaultIndex from './defaultIndex.vue'
+import FriendsIndex from './friendsIndex.vue'
+
+const userinfo = useStorage('userinfo', { token: '' })
+const currentPage = computed(() => (userinfo.value.token ? DefaultIndex : FriendsIndex))
 </script>
